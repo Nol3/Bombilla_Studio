d4d624f628ebaa37bc0f577977b619b23f6ff8d1
Alejandro Cárdenas Parejo <alejandrocp_952@hotmail.com>
2024-01-27 00:08:48 +0100

    player-test

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,72 +1,103 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.Burst.Intrinsics;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    /*
-        float h;
-        float v;
-        Vector3 move;
-        [SerializeField] private float speed = 5;
-        //[SerializeField] Transform aim;
-        //[SerializeField] Camera cam;
-        Vector2 facingDirection;
-       // [SerializeField] Transform bulletPrefab;
-        bool gunLoaded = true;
-        [SerializeField] float fireRate = 1;
-        [SerializeField] int Health = 5;
-        [SerializeField] bool PowerShot;
+    float h;
+    float v;
+    Vector3 move;
+    [SerializeField] private float speed = 5;
+    [SerializeField] Transform aim;
+    Vector2 facingDirection;
+    [SerializeField] Transform bulletPrefab;
+    bool gunLoaded = true;
+    [SerializeField] float fireRate = 1;
+    [SerializeField] int Health = 5;
+    [SerializeField] bool PowerShot;
 
-        // Start is called before the first frame update
-        void Start()
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+
+        move.x = h;
+        move.y = v;
+
+        transform.position += move * Time.deltaTime * speed;
+        
+        //movimiento del arma
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition.z = transform.position.z - Camera.main.transform.position.z;
+        facingDirection = Camera.main.ScreenToWorldPoint(mousePosition) - transform.position;
+        aim.position = transform.position + (Vector3)facingDirection.normalized;
+
+        if (Input.GetMouseButton(0) && gunLoaded)
         {
-
+            gunLoaded = false;
+            float angle = Mathf.Atan2(facingDirection.y, facingDirection.x) * Mathf.Rad2Deg;
+            Quaternion targetRotation = Quaternion.AngleAxis(angle, Vector3.forward);
+            Instantiate(bulletPrefab, transform.position, targetRotation);
+            StartCoroutine(ReloadGun());
         }
+    }
 
-        // Update is called once per frame
-        void Update()
+    IEnumerator ReloadGun()
+    {
+        yield return new WaitForSeconds(1 / fireRate);
+        gunLoaded = true;
+    }
+
+    public void TakeDamage()
+    {
+        Health--;
+        if (Health < 0)
         {
-            h = Input.GetAxis("Horizontal");
-            v = Input.GetAxis("Vertical");
+            // Game over
+        }
+    }
 
-            move.x = h;
-            move.y = v;
 
-            transform.position += move * Time.deltaTime * speed;
-
-            //aim movement
-            //facingDirection = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-            //aim.position = transform.position + facingDirection.normalized;
-
-            /* Vector3 mousePosition = Input.mousePosition;
-             mousePosition.z = transform.position.z - Camera.main.transform.position.z;
-             facingDirection = Camera.main.ScreenToWorldPoint(mousePosition) - transform.position;
-             aim.position = transform.position + (Vector3)facingDirection.normalized;
-
-             if (Input.GetMouseButton(0) && gunLoaded)
-             {
-                 gunLoaded = false;
-                 float angle = Mathf.Atan2(facingDirection.y, facingDirection.x) * Mathf.Rad2Deg;
-                 Quaternion targetRotation = Quaternion.AngleAxis(angle, Vector3.forward);
-                 Instantiate(bulletPrefab, transform.position, targetRotation);
-                 StartCoroutine(ReloadGun());
-             }
-         }
-         IEnumerator ReloadGun()
-         {
-             yield return new WaitForSeconds(1 / fireRate);
-             gunLoaded = true;
-         }
-
-         public void TakeDamage()
-         {
-             Health--;
-             if (Health < 0)
-             {
-                 // Game over
-             }
-
-        }*/
+//    private void OnTriggerEnter2D(Collider2D collision)
+//    {
+//        if (collision.CompareTag("PowerUp"))
+//        {
+//            switch (collision.GetComponent<PowerUp>().powerUpType)
+//            {
+//                case PowerUp.PowerUpType.FireRateIncrease:
+//                    {
+//                        fireRate++;
+//                        break;
+//                    }
+//                case PowerUp.PowerUpType.PowerShot:
+//                    {
+//                        PowerShot = true;
+//                        break;
+//                    }
+//                //case PowerUp.PowerUpType.SpeedBoost:
+//                //    {
+//                //        // + velocidad
+//                //        break;
+//                //    }
+//                //case PowerUp.PowerUpType.HealthBoost:
+//                //    {
+//                //        // + salud
+//                //        break;
+//                //    }
+//                default:
+//                    {
+//                        // Caso por defecto si no coincide con ningún tipo de power-up conocido
+//                        break;
+//                    }
+//            }
+//        }
+//    }
 }
