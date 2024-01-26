ec8bd108679ed537473f831184b8beac0ac56855
achade14161 <120469220+achade14161@users.noreply.github.com>
2024-01-26 20:33:30 +0100

    Noa-1

---
+++ Assets/Scenes/Scripts/Enemigos/Spawners.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] GameObject enemyPrefab;
+    [Range(1, 10)][SerializeField] float ratio = 1;
+
+    IEnumerator Spawner()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1 / ratio);
+            Instantiate(enemyPrefab);
+        }
+
+    }
+
+}
+
+
