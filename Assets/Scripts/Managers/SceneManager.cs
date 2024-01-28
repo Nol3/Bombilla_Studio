e9f64185f2551ef1101e485767233b20334be3ae
achade14161 <120469220+achade14161@users.noreply.github.com>
2024-01-28 02:26:13 +0100

    Noa Intento Scene manager

---
+++ Assets/Scripts/Managers/SceneManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneManager : MonoBehaviour
+{
+    //Manejo de escenas
+
+    public void Inicio_Selector()
+    {
+        SceneManager.LoadScene("2-Selector");
+    }
+
+    public void Selector_Instrucciones()
+    {
+        SceneManager.LoadScene("3-Instrucciones");
+    }
+
+    public void Instrucciones_Game()
+    {
+        SceneManager.LoadScene("4-Game");
+    }
+
+    public void Replay()
+    {
+        SceneManager.LoadScene("1-Inicio");
+    }
+
+}
