f9cb8e32b3d9bf40dc754dab22268b40b34c358d
Aramrot <maranon.art@gmail.com>
2024-01-28 07:58:19 +0100

    asa

--- Assets/Scripts/GameLoads.cs
+++ Assets/Scripts/GameLoads.cs
@@ -1,16 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameLoads : MonoBehaviour
 {
+    public TextMeshProUGUI Obj_Text_Top;
+    public TextMeshProUGUI Obj_Text_Bot;
+    public Image MemeSprite;
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log(PlayerPrefs.GetString("top_text"));
-        Debug.Log(PlayerPrefs.GetString("bot_text"));
+        /*Debug.Log(PlayerPrefs.GetString("top_text"));
+        Debug.Log(PlayerPrefs.GetString("bot_text"));*/
         Debug.Log(PlayerPrefs.GetString("meme_name"));
+
+        Obj_Text_Top.text = PlayerPrefs.GetString("top_text");
+        Obj_Text_Bot.text = PlayerPrefs.GetString("bot_text");
+        string MemeName = PlayerPrefs.GetString("meme_name");
+        Sprite loadedImage = Resources.Load<Sprite>(MemeName);
+        MemeSprite.sprite = loadedImage;
+
     }
 
-    
+
 }
