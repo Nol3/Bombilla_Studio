4ba3dd5d2cdb9ff2222529b4823e261b8e0b6586
Aramrot <maranon.art@gmail.com>
2024-01-28 04:01:59 +0100

    xxxx

--- Assets/Scripts/CharacterOverView.cs
+++ Assets/Scripts/CharacterOverView.cs
@@ -39,32 +39,34 @@ public class CharacterOverView : MonoBehaviour
 
         }
     }
-    
+
     public void ResetText()
     {
         Obj_Text_Top.text = "";
         Obj_Text_Bot.text = "";
     }
 
-  public void SumbitMeme()
-{
-    PlayerPrefs.SetString("top_text", Obj_Text_Top.text);
-    PlayerPrefs.SetString("bot_text", Obj_Text_Bot.text);
-    PlayerPrefs.SetString("meme_name", FinalMeme.sprite.name);
-    PlayerPrefs.Save();
-    StartCoroutine(LoadSceneGameAfterDelay(1));
-}
+    public void SumbitMeme()
+    {
+        PlayerPrefs.SetString("top_text", Obj_Text_Top.text);
+        PlayerPrefs.SetString("bot_text", Obj_Text_Bot.text);
+        PlayerPrefs.SetString("meme_name", FinalMeme.sprite.name);
+        PlayerPrefs.Save();
+        StartCoroutine(LoadSceneGameAfterDelay(1));
+    }
 
-private IEnumerator LoadSceneGameAfterDelay(float delay)
-{
-    yield return new WaitForSeconds(delay);
-    LoadSceneGame();
-}
-
+    private IEnumerator LoadSceneGameAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        LoadSceneGame();
+    }
     public void LoadSceneGame()
     {
         Debug.Log("cosa");
-        SceneManager.LoadScene(3);
+        Scene sceneLoaded = SceneManager.GetActiveScene();
+
+
     }
+
 
 }
